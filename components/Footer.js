2545c650d7c3d95fbd801fd4a70b69f5e421eecd
ccyunoo <ccyunoo@gmail.com>
2023-04-13 10:45:38 +0800

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -18,8 +18,7 @@ const Footer = ({ fullWidth }) => {
           <p>
             © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`}
           </p>
-          // <Vercel />
-          Powered by Vercel
+          <Vercel />
         </div>
       </div>
     </div>
