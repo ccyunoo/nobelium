29b71ec76fc3b95605d605bb410a6dd6464f64cf
ccyunoo <ccyunoo@gmail.com>
2023-10-11 17:14:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: '看云卷云舒 Ccyun.com',
+  title: '看云卷云舒 Xiaoyun Blog',
   author: 'Xiaoyun',
   email: 'ccyunoo@gmail.com',
   link: 'http://www.ccyun.com',
-  description: 'Xiaoyun Blog',
+  description: 'Ccyun.com',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
