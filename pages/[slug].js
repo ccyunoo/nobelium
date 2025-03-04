a75e0376bbbf1d6150da716fe75622d17094ae37
ccyunoo <ccyunoo@gmail.com>
2025-03-04 15:28:35 +0800

    update

--- pages/[slug].js
+++ pages/[slug].js
@@ -41,7 +41,7 @@ export default function BlogPost ({ post, blockMap, emailHash }) {
       <div
         className={cn(
           'px-4 flex justify-between font-medium text-gray-500 dark:text-gray-400 my-5',
-          fullWidth ? 'md:px-24' : 'mx-auto max-w-6xl'
+          fullWidth ? 'md:px-24' : 'mx-auto max-w-4xl'
         )}
       >
         <a>
