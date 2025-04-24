fdb96a5dd6385c71171d4528845fd9bbf9ee9a4a
ccyunoo <ccyunoo@gmail.com>
2025-04-24 15:59:50 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,10 @@
-import { config } from './lib/server/config'
-import { FONTS_SANS, FONTS_SERIF } from './consts'
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js'],
-  darkMode: 'class',
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {
-      colors: {
-        day: {
-          DEFAULT: config.lightBackground || '#ffffff'
-        },
-        night: {
-          DEFAULT: config.darkBackground || '#111827'
-        }
-      },
-      fontFamily: {
-        sans: FONTS_SANS,
-        serif: FONTS_SERIF,
-        noEmoji: [
-          '"IBM Plex Sans"',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'sans-serif'
-        ]
-      }
-    }
+    extend: {},
   },
-  variants: {
-    extend: {}
-  },
-  plugins: []
+  plugins: [],
 }
