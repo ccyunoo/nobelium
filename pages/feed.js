aba05466897683b2c448ef80d1a5f308a971df8a
ccyunoo <ccyunoo@gmail.com>
2026-08-21 10:54:47 +0800

    update test1

--- pages/feed.js
+++ pages/feed.js
@@ -2,11 +2,18 @@ import { getAllPosts } from '@/lib/notion'
 import { generateRss } from '@/lib/rss'
 export async function getServerSideProps ({ res }) {
   res.setHeader('Content-Type', 'text/xml')
-  const posts = await getAllPosts({ includePages: false })
-  const latestPosts = posts.slice(0, 10)
-  const xmlFeed = await generateRss(latestPosts)
-  res.write(xmlFeed)
-  res.end()
+  try {
+    const posts = await getAllPosts({ includePages: false })
+    const latestPosts = posts.slice(0, 10)
+    const xmlFeed = await generateRss(latestPosts)
+    res.write(xmlFeed)
+    res.end()
+  } catch (err) {
+    console.error('Failed to generate /feed:', err)
+    const xmlFeed = await generateRss([])
+    res.write(xmlFeed)
+    res.end()
+  }
   return {
     props: {}
   }
